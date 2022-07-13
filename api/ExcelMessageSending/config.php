b065bddb69b60b34ea201ce251785ef8e645f554
aVr0Ra <32928798+aVr0Ra@users.noreply.github.com>
2022-07-13 15:15:39 +0800

    Update config.php

--- api/ExcelMessageSending/config.php
+++ api/ExcelMessageSending/config.php
@@ -13,12 +13,12 @@
 
 return array (
     // 企业的id，在管理端->"我的企业" 可以看到
-    "CORP_ID"               => "ww0df921873f4c8010",
+    "CORP_ID"               => "",
 
     // "通讯录同步"应用的secret, 开启api接口同步后，可以在管理端->"通讯录同步"看到
     "CONTACT_SYNC_SECRET"   => "",
 
     // 某个自建应用的id及secret, 在管理端 -> 企业应用 -> 自建应用, 点进相应应用可以看到
-    "APP_ID"                => 1000003,
-    "APP_SECRET"            => "yD4kmh3HJGSFNF8R9SMXPPlKrLgFgDqfLtXGDMhSjmI",
+    "APP_ID"                => ,
+    "APP_SECRET"            => "",
 );
